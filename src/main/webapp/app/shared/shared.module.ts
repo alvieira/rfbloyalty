e7e75e15212f0506085635fe5833b373ba415ade
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-09-24 23:13:30 -0300

    Initial version of rfbloyalty generated by JHipster-6.10.3

---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RfbloyaltySharedLibsModule } from './shared-libs.module';
+import { AlertComponent } from './alert/alert.component';
+import { AlertErrorComponent } from './alert/alert-error.component';
+import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
+
+@NgModule({
+  imports: [RfbloyaltySharedLibsModule],
+  declarations: [AlertComponent, AlertErrorComponent, HasAnyAuthorityDirective],
+  exports: [RfbloyaltySharedLibsModule, AlertComponent, AlertErrorComponent, HasAnyAuthorityDirective],
+})
+export class RfbloyaltySharedModule {}
